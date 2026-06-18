521d154986bf44559846c232dffb4fa895b4f76b
jangbri <jangbri@gmail.com>
2026-06-17 20:01:51 -0400

    feat: trips endpoint

--- backend/internal/transit/mta/trips/repo.go
+++ backend/internal/transit/mta/trips/repo.go
@@ -1,6 +1,11 @@
 package trips
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"log/slog"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type TripRepo struct {
 	db *pgxpool.Pool
@@ -8,4 +13,67 @@ type TripRepo struct {
 
 func NewTripRepo(db *pgxpool.Pool) *TripRepo {
 	return &TripRepo{db: db}
+}
+
+func (r *TripRepo) GetAll(ctx context.Context) ([]*Trip, error) {
+	stmt := `
+		SELECT
+			id, route_id, service_id, headsign, direction_id, shape_id
+		FROM
+			trips
+	`
+
+	rows, err := r.db.Query(ctx, stmt)
+	if err != nil {
+		slog.Error("query trips table", "err", err)
+		return nil, err
+	}
+
+	trips := []*Trip{}
+
+	for rows.Next() {
+		var trip Trip
+		if err := rows.Scan(
+			&trip.ID,
+			&trip.RouteID,
+			&trip.ServiceID,
+			&trip.HeadSign,
+			&trip.DirectionID,
+			&trip.ShapeID,
+		); err != nil {
+			slog.Error("retrieving particular row", "err", err)
+		}
+		trips = append(trips, &trip)
+	}
+
+	return trips, nil
+}
+
+func (r *TripRepo) GetTrip(ctx context.Context, id string) (*Trip, error) {
+	stmt := `
+		SELECT
+			id, route_id, service_id, headsign, direction_id, shape_id
+		FROM
+			trips
+		WHERE
+			id = $1
+	`
+
+	var trip Trip
+
+	row := r.db.QueryRow(ctx, stmt, id)
+	err := row.Scan(
+		&trip.ID,
+		&trip.RouteID,
+		&trip.ServiceID,
+		&trip.HeadSign,
+		&trip.DirectionID,
+		&trip.ShapeID,
+	)
+	if err != nil {
+		slog.Error("retrieving row with specific id", "id", id, "err", err)
+		return nil, err
+	}
+
+	return &trip, nil
 }
--- backend/internal/transit/mta/trips/service.go
+++ backend/internal/transit/mta/trips/service.go
@@ -1,9 +1,19 @@
 package trips
 
+import "context"
+
 type TripService struct {
 	tripRepo *TripRepo
 }
 
 func NewTripService(tr *TripRepo) *TripService {
 	return &TripService{tripRepo: tr}
+}
+
+func (s *TripService) GetAll(ctx context.Context) ([]*Trip, error) {
+	return s.tripRepo.GetAll(ctx)
+}
+
+func (s *TripService) GetTrip(ctx context.Context, id string) (*Trip, error) {
+	return s.tripRepo.GetTrip(ctx, id)
 }
